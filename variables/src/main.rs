f90c4385bc539a197098f705b58a1652815f852d
Ilay Katz <ilayk98@gmail.com>
2023-06-08 18:07:24 +0300

    chapter 3: variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,12 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 //MUTABLE VAR
-// fn main() {
-//     let mut x = 5;
-//     println!("The value of x is {x}");
-//     x = 6;
-//     println!("The value of x is {x}");
-// }
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+}
 
 //SHADOWING
 // fn main() {

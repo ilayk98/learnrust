844ea0d4d8c14d6ab2a4b4eb554ea9f3f3c8eb0c
Ilay Katz <ilayk98@gmail.com>
2023-06-10 10:29:21 +0300

    chapter3: control flow

---
+++ branches/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    // let number = 6;
+
+    // if number % 4 == 0 {
+    //     println!("Number is divisible by 4");
+    // } else if number % 3 == 0 {
+    //     println!("Number is divisible by 3");
+    // } else if number % 2 == 0 {
+    //     println!("Number is divisible by 2");
+    // } else  {
+    //     println!("Number is not divisible by 4, 3 or 2");
+    // }
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // } else {
+    //     println!("Hello, world!");
+    // }
+}

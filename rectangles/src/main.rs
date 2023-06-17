a5a1db639adeeedc29af6cee4686207fbcc6c9ce
Ilay Katz <ilayk@gmail.com>
2023-06-17 03:09:24 +0300

    chapter5.2(structs): continue rectangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -13,6 +13,8 @@
 //     width * height
 // }
 
+/////////////////////////////////////////////////////////////// 
+
 // TUPLE WAY
 // fn main() {
 //     let rect1 = (30, 50);
@@ -26,3 +28,31 @@
 //     dimensions.0 * dimensions.1
 // }
 
+// ////////////////////////////////////////////////////////////
+
+// STRUCT WAY
+#[derive(Debug)]
+struct Rectangle {
+	width: u32,
+	height: u32,
+}
+
+fn main() {
+	let rect1 = Rectangle {
+		width: 30,
+		height: 50,
+	};
+
+	println!("rect1 is {:#?}", rect1);
+	println!("rect1 is {:?}", rect1);
+	dbg!(&rect1);
+
+	println!(
+		"The area of the rectangle is {} square pixels.",
+		area(&rect1)
+	);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+	rectangle.width * rectangle.height
+}
